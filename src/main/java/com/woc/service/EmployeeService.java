4b660ae3cb358dd38e2ddeef7932eb5833e44164
ViVXone <vivx.xone@gmail.com>
2019-11-16 10:24:09 +0530

    Adding validation for Empty Email

--- src/main/java/com/woc/service/EmployeeService.java
+++ src/main/java/com/woc/service/EmployeeService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeService {
     public Employee fetchEmployee(String email){
-
+        if(email.length() == 0){
+            throw new RuntimeException("Email Should not be blank");
+        }
         //fetch the data for given email from db
         Employee employee = new Employee();
         employee.setId(500);
